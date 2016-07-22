6af77308d5732f14779a36b90dcac1f2ce9f7d12
Sergey Popov <100003047626231@facebook.com>
2016-07-22 14:34:53 +0300

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Sergey on 7/22/16.
+//  Copyright © 2016 Sergey Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Sergey on 7/22/16.
+//  Copyright © 2016 Sergey Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
